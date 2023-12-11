1c2e8422078f8c6f73e18848ab7f7efc1ba930e6
umesh <umeshkec364@gmail.com>
2023-12-11 20:02:16 +0530

    done

--- practise/index.js
+++ practise/index.js
@@ -1,4 +1,23 @@
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
+// numbers.forEach(function (number) {
+//   console.log(number);
+// });
+// const colors = ['red', 'green', 'blue'];
+
+// for (const color of colors) {
+//   console.log(color);
+// }
+// const person = {
+//   name: 'Alice',
+//   age: 30,
+//   job: 'Engineer'
+// };
+
+// for (const key in person) {
+//   console.log(key, person[key]);
+// }
+const numbers = [1, 2, 3, 4, 5];
+
 numbers.forEach(function (number) {
   console.log(number);
 });
